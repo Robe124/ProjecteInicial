3aafba90f0f373a2687a1ac6ec2837a4cf578ecd
Robert.Marti <104197815+Robe124@users.noreply.github.com>
2024-10-16 20:32:24 +0200

    DockerArchius

--- backend/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<h1>Bienvenido al sistema de backend de Antica Roma Ristorante</h1>";
-?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+  echo "<h1>¡Hola, esta es mi página web!</h1>";
+  echo "<p>Este contenido está siendo servido desde PHP</p>";
+?>
