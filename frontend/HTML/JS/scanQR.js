fa05d3815154252ab525b0a0de444fb4c714f9f9
Robert.Marti <104197815+Robe124@users.noreply.github.com>
2024-10-21 18:58:10 +0200

    pruebasQRningunCambioEnconcreto

---
+++ frontend/HTML/JS/scanQR.js
@@ -0,0 +1,13 @@
+ 
+document.getElementById("Boton-Volver").onclick = function() {
+    window.location.href = "./index.html";
+  };
+
+  Dynamsoft.DBR.BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
+  let scanner = null;
+  (async()=>{
+      scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
+      scanner.onFrameRead = results => {console.log(results);};
+      scanner.onUnduplicatedRead = (txt, result) => {};
+      await scanner.show();
+  })();
