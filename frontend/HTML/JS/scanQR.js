a9028d1d41ff0a94e3eb5973726b96afa7b5f183
Robert.Marti <104197815+Robe124@users.noreply.github.com>
2024-10-21 19:13:05 +0200

    prueba2QRscan

--- frontend/HTML/JS/scanQR.js
+++ frontend/HTML/JS/scanQR.js
@@ -1,13 +1,15 @@
- 
 document.getElementById("Boton-Volver").onclick = function() {
     window.location.href = "./index.html";
-  };
+};
 
-  Dynamsoft.DBR.BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
-  let scanner = null;
-  (async()=>{
-      scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
-      scanner.onFrameRead = results => {console.log(results);};
-      scanner.onUnduplicatedRead = (txt, result) => {};
-      await scanner.show();
-  })();
+Dynamsoft.DBR.BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yciI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
+let scanner = null;
+
+(async () => {
+    scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
+    scanner.onFrameRead = results => { console.log(results); };
+    scanner.onUnduplicatedRead = (txt, result) => {
+        window.location.href = "https://anticaromaristorante.netlify.app/OpcionsCompra";
+    };
+    await scanner.show();
+})();
